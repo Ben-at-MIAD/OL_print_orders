a264ec7de7a0d2911ca2e9cafdaf044cdce07cb6
Benjamin-Dembroski <benjamin-dembroski@webdev17.04>
2017-06-09 10:31:15 -0500

    initial models outlined

--- printorders/Lulz_orders/models.py
+++ printorders/Lulz_orders/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Filament(models.Model):
+    # cost per gram
+    # Color
+    # rolls in inventory
+    #
+class Order(models.Model)
+    # stl file
+    # gcode file
+    # date order placed
+    # order cost
+    # number of grams
+    # payment status
+    # date completed
+    # client
+    # class
+    # filament
+    # collection date
+
+class STL(models.Model):
+    # stl
+
+class Gcode(models.Model):
+    # gcodefile
+
+class Student(models.Model):
+
+class Faculty(models.Model):
+
+class OL_staff(models.Model):
