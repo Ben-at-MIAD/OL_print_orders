8006e09864d4902afeee4c85108d25f3ca55d3ce
Benjamin-Dembroski <benjamin-dembroski@webdev17.04>
2017-06-14 13:12:16 -0500

    changed settings.py for media uploads

--- printorders/printorders/settings.py
+++ printorders/printorders/settings.py
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+### Ben added this for file handling
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
