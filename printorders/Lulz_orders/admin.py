6a9b2142e9e969b8cee4bbfd941fdbdb3e844e8b
Benjamin-Dembroski <benjamin-dembroski@webdev17.04>
2017-06-14 13:03:15 -0500

    tweaking models..

--- printorders/Lulz_orders/admin.py
+++ printorders/Lulz_orders/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Order, Filament, FilamentBrand, FilamentMaterialType
+
 # Register your models here.
+
+admin.site.register(Order)
+admin.site.register(Filament)
+admin.site.register(FilamentBrand)
+admin.site.register(FilamentMaterialType)
--- printorders/Lulz_orders/models.py
+++ printorders/Lulz_orders/models.py
@@ -1,27 +1,47 @@
 from django.db import models
 from django.core.files.storage import FileSystemStorage
 
-fs = FileSystemStorage(location='/files/')
-
 # Create your models here.
+
+
 class STL(models.Model):
-    stl_file = models.FileField(storage=fs)
+    stl_file = models.FileField(upload_to="stl/")
 
 class Gcode(models.Model):
-    gcode_file = models.FileField(storage=fs)
+    gcode_file = models.FileField(upload_to="gcode/")
+
+class FilamentBrand(models.Model):
+    name = models.CharField(max_length = 15)
+
+    def __str__(self):
+        return self.name
+
+class FilamentMaterialType(models.Model):
+    name = models.CharField(max_length=6)
+
+    def __str__(self):
+        return self.name
 
 class Filament(models.Model):
+
     #color =models.CharField(max_length=50)
     #inventory = models.IntegerField()
     #weight = models.IntegerField()
     #rolls_used = models.IntegerField()
     cost =  models.DecimalField(max_digits=4, decimal_places=2)
     markup = models.DecimalField(max_digits=2, decimal_places=2)
-    # plastic type
-    # Brand
+    material = models.ForeignKey(FilamentMaterialType, default=1)
+    brand =  models.ForeignKey(FilamentBrand, default=1)
+
+    def __str__(self):
+        return '%s %s' % (self.brand, self.material)
     # Vendor
 
+class Print(models.Model):
+    name =
+
 class Order(models.Model):
+    submit_datetime = DateTimeField(auto_now_add=True)
     stl = models.ForeignKey(STL, null=True)
     filament = models.ForeignKey(Filament, null=True)
     gcode = models.ForeignKey(Gcode, null=True)
@@ -30,7 +50,8 @@ class Order(models.Model):
     grams = models.PositiveIntegerField(null=True)
     # payment status
     # date completed
-    # client
+    client = models.ForeignKey(User)
+    tech = models.ForeignKey(User)
     # class
     # filament
     # collection date
