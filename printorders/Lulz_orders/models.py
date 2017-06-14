3bf3409e23a818b9165a7fe4efb06794387e4dd7
Benjamin-Dembroski <benjamin-dembroski@webdev17.04>
2017-06-14 10:49:52 -0500

    initial migration, and sundry settings

--- printorders/Lulz_orders/models.py
+++ printorders/Lulz_orders/models.py
@@ -1,19 +1,33 @@
 from django.db import models
+from django.core.files.storage import FileSystemStorage
+
+fs = FileSystemStorage(location='/files/')
 
 # Create your models here.
+class STL(models.Model):
+    stl_file = models.FileField(storage=fs)
 
+class Gcode(models.Model):
+    gcode_file = models.FileField(storage=fs)
 
 class Filament(models.Model):
-    # cost per gram
-    # Color
-    # rolls in inventory
-    #
-class Order(models.Model)
-    # stl file
-    # gcode file
-    # date order placed
-    # order cost
-    # number of grams
+    #color =models.CharField(max_length=50)
+    #inventory = models.IntegerField()
+    #weight = models.IntegerField()
+    #rolls_used = models.IntegerField()
+    cost =  models.DecimalField(max_digits=4, decimal_places=2)
+    markup = models.DecimalField(max_digits=2, decimal_places=2)
+    # plastic type
+    # Brand
+    # Vendor
+
+class Order(models.Model):
+    stl = models.ForeignKey(STL, null=True)
+    filament = models.ForeignKey(Filament, null=True)
+    gcode = models.ForeignKey(Gcode, null=True)
+    cost =  models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    grams = models.PositiveIntegerField(null=True)
     # payment status
     # date completed
     # client
@@ -21,14 +35,12 @@ class Order(models.Model)
     # filament
     # collection date
 
-class STL(models.Model):
-    # stl
 
-class Gcode(models.Model):
-    # gcodefile
 
-class Student(models.Model):
+#class Student(models.Model):
 
-class Faculty(models.Model):
+#class Faculty(models.Model):
 
-class OL_staff(models.Model):
+#class OL_staff(models.Model):
+
+#class Course(models.Model):
--- printorders/printorders/settings.py
+++ printorders/printorders/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Lulz_orders.apps.LulzOrdersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
